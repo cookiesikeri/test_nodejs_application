9eeea9c804993370a5e999668b42fef424a9e13a
Dinesh <51910892+dineshindn@users.noreply.github.com>
2021-08-30 07:23:08 +0530

    Update db.config.js

--- db.config.js
+++ db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     connectionLimit :10,
     host : 'localhost',
-    user: 'root',
-    password: '',
-    database:'test_databases',
+    user: 'test_user',
+    password: 'Admin@2021',
+    database:'test_database',
     port: 4000,
 }
 
