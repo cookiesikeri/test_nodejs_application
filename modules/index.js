bac686e1307ff817ea6173ae5146eed1ffa21e62
Dinesh <51910892+dineshindn@users.noreply.github.com>
2021-08-30 07:28:57 +0530

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -4,7 +4,7 @@ module.exports = {
   create: async (req, res) => {
     try {
       const result = await query(
-        "INSERT INTO Users (userName, password, role, address, email) VALUES (? ,? ,?, ?, ?)",
+        "INSERT INTO users (userName, password, role, address, email) VALUES (? ,? ,?, ?, ?)",
         [req.body.userName, req.body.password, req.body.role, req.body.address, req.body.email]
       );
       return res.status(200).send({
@@ -21,7 +21,7 @@ module.exports = {
   list: async (req, res) => {
     try {
       const id = req.body.id;
-      if (id) {query1 = `SELECT * FROM Users WHERE id = ${id}`;} 
+      if (id) {query1 = `SELECT * FROM users WHERE id = ${id}`;} 
       else {query1 = "SELECT * FROM Users ";}
       const result = await query(query1, (err, rows) => {
         if (err) { throw err;}
